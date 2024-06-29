786c0e08598f8d5b9bde3c8340508a84ea02bc57
XsystemH <1319971303@qq.com>
2024-06-29 12:05:07 +0800

    Fuzzed_Metal

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -19,15 +19,15 @@ use std::sync::Arc;
 use std::{fs::File, process::exit};
 
 fn main() {
-    let path = std::path::Path::new("output/book1/image13.jpg");
+    let path = std::path::Path::new("output/book1/image14.jpg");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
     // Materials
     let material_ground = Arc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let material_center = Arc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Arc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
-    let material_right = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
+    let material_left = Arc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    let material_right = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     // World
     let mut world: HittableList = HittableList::new();
--- raytracer/src/material.rs
+++ raytracer/src/material.rs
@@ -1,7 +1,7 @@
 use crate::color::Color;
 use crate::hittable::HitRecord;
 use crate::ray::Ray;
-use crate::vec3::{random_unit_vector, reflect};
+use crate::vec3::{dot, random_unit_vector, reflect, unit_vector};
 
 pub trait Material: Send + Sync {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Ray, Color)>;
@@ -35,19 +35,25 @@ impl Material for Lambertian {
 #[derive(Clone)]
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        Self { albedo, fuzz }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Ray, Color)> {
-        let reflected = reflect(&r_in.direction(), &rec.normal);
+        let mut reflected = reflect(&r_in.direction(), &rec.normal);
+        reflected = unit_vector(&reflected) + random_unit_vector() * self.fuzz;
         let scattered = Ray::new(&rec.p, &reflected);
         let attenuation = self.albedo.clone();
-        Some((scattered, attenuation))
+        if dot(&scattered.direction(), &rec.normal) > 0.0 {
+            Some((scattered, attenuation))
+        } else {
+            None
+        }
     }
 }
