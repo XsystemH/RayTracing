a5a060109d1fdb9fc6a4831b7309cdd9ec7f577f
XsystemH <1319971303@qq.com>
2024-07-04 16:22:49 +0800

    Final_Scene

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -343,16 +343,7 @@ fn quads() {
     exit(0);
 }
 
-fn main() {
-    if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
-        bouncing_spheres();
-    } else if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
-        earth();
-    } else if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
-        perlin();
-    } else if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
-        quads();
-    }
+fn cornell_box() {
     let path = std::path::Path::new("output/book2/image22.jpg");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
@@ -458,108 +449,145 @@ fn main() {
     exit(0);
 }
 
-// fn main() {
-//     if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
-//         bouncing_spheres();
-//     } else if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
-//         earth();
-//     } else if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
-//         perlin();
-//     } else if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
-//         quads();
-//     } else if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
-//         cornell_box();
-//     }
-//     let path = std::path::Path::new("output/book2/image22.jpg");
-//     let prefix = path.parent().unwrap();
-//     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
-//
-//     let mut boxes1 = HittableList::new();
-//     let ground = Arc::new(Lambertian::new(Color::new(0.48, 0.83, 0.53)));
-//
-//     let boxes_per_sides = 20;
-//     for i in 0..boxes_per_sides {
-//         for j in 0..boxes_per_sides {
-//             let w = 100.0;
-//             let x0 = -1000.0 + i as f64 * w;
-//             let y0 = 0.0;
-//             let z0 = -1000.0 + j as f64 * w;
-//             let x1 = x0 + w;
-//             let y1 = thread_rng().gen_range(1.0..101.0);
-//             let z1 = z0 + w;
-//
-//             boxes1.add(cuboid(&Point3::new(x0, y0, z0), &Point3::new(x1, y1, z1), ground.clone()));
-//         }
-//     }
-//
-//     let mut world = HittableList::new();
-//     world.add(Arc::new(BvhNode::from_list(&mut Arc::new(boxes1))));
-//
-//     let light = Arc::new(DiffuseLight::new(&Color::new(7.0, 7.0, 7.0)));
-//     world.add(Arc::new(Quad::new(
-//         &Point3::new(123.0, 554.0, 147.0),
-//         &Vec3::new(300.0, 0.0, 0.0),
-//         &Vec3::new(0.0, 0.0, 265.0),
-//         light
-//     )));
-//
-//     let center1 = Point3::new(400.0, 400.0, 200.0);
-//     let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
-//     let sphere_material = Arc::new(Lambertian::new(Color::new(0.7, 0.3, 0.1)));
-//     world.add(Arc::new(Sphere::moving(&center1, 50.0, sphere_material, &center2)));
-//
-//     world.add(Arc::new(Sphere::new(
-//         &Point3::new(260.0, 150.0, 45.0),
-//         50.0,
-//         Arc::new(Dielectric::new(1.5)),
-//     )));
-//     world.add(Arc::new(Sphere::new(
-//         &Point3::new(0.0, 150.0, 145.0),
-//         50.0,
-//         Arc::new(Metal::new(Color::new(0.8, 0.8, 0.9), 1.0)),
-//     )));
-//
-//     let boundary = Arc::new(Sphere::new(
-//         &Point3::new(360.0, 150.0, 145.0), 70.0, Arc::new(Dielectric::new(1.5))));
-//     world.add(boundary);
-//     world.add(Arc::new(Cons))
-//
-//     let world = HittableList::new_from(Arc::new(BvhNode::from_list(&mut world)));
-//
-//     let image_settings = ImageSettings {
-//         aspect_ratio: 1.0,
-//         image_width: 600,
-//         quality: 100,
-//         samples_per_pixel: 200,
-//         max_depth: 50,
-//         background: Color::black(),
-//     };
-//
-//     let camera_settings = CameraSettings {
-//         vfov: 40.0,
-//         look_from: Point3::new(278.0, 278.0, -900.0),
-//         look_at: Point3::new(278.0, 278.0, 0.0),
-//         vup: Vec3::new(0.0, 1.0, 0.0),
-//         defocus_angle: 0.0,
-//         focus_dist: 10.0,
-//     };
-//
-//     let mut camera = Camera::new(image_settings, camera_settings);
-//     camera.render(world);
-//
-//     println!(
-//         "Output image as \"{}\"",
-//         style(path.to_str().unwrap()).yellow()
-//     );
-//     let output_image = image::DynamicImage::ImageRgb8(camera.img);
-//     let mut output_file = File::create(path).unwrap();
-//     match output_image.write_to(
-//         &mut output_file,
-//         image::ImageOutputFormat::Jpeg(camera.quality),
-//     ) {
-//         Ok(_) => {}
-//         Err(_) => println!("{}", style("Outputting image fails.").red()),
-//     }
-//
-//     exit(0);
-// }
+fn main() {
+    if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
+        bouncing_spheres();
+    } else if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
+        earth();
+    } else if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
+        perlin();
+    } else if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
+        quads();
+    } else if thread_rng().gen_range(0.0..1.0) < 0.0000001 {
+        cornell_box();
+    }
+    let path = std::path::Path::new("output/book2/image23.jpg");
+    let prefix = path.parent().unwrap();
+    std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
+
+    let mut boxes1 = HittableList::new();
+    let ground = Arc::new(Lambertian::new(Color::new(0.48, 0.83, 0.53)));
+
+    let boxes_per_sides = 20;
+    for i in 0..boxes_per_sides {
+        for j in 0..boxes_per_sides {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64 * w;
+            let y0 = 0.0;
+            let z0 = -1000.0 + j as f64 * w;
+            let x1 = x0 + w;
+            let y1 = thread_rng().gen_range(1.0..101.0);
+            let z1 = z0 + w;
+
+            boxes1.add(cuboid(&Point3::new(x0, y0, z0), &Point3::new(x1, y1, z1), ground.clone()));
+        }
+    }
+
+    let mut world = HittableList::new();
+    world.add(Arc::new(BvhNode::from_list(&mut boxes1)));
+
+    let light = Arc::new(DiffuseLight::new(&Color::new(7.0, 7.0, 7.0)));
+    world.add(Arc::new(Quad::new(
+        &Point3::new(123.0, 554.0, 147.0),
+        &Vec3::new(300.0, 0.0, 0.0),
+        &Vec3::new(0.0, 0.0, 265.0),
+        light
+    )));
+
+    let center1 = Point3::new(400.0, 400.0, 200.0);
+    let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
+    let sphere_material = Arc::new(Lambertian::new(Color::new(0.7, 0.3, 0.1)));
+    world.add(Arc::new(Sphere::moving(&center1, 50.0, sphere_material, &center2)));
+
+    world.add(Arc::new(Sphere::new(
+        &Point3::new(260.0, 150.0, 45.0),
+        50.0,
+        Arc::new(Dielectric::new(1.5)),
+    )));
+    world.add(Arc::new(Sphere::new(
+        &Point3::new(0.0, 150.0, 145.0),
+        50.0,
+        Arc::new(Metal::new(Color::new(0.8, 0.8, 0.9), 1.0)),
+    )));
+
+    let mut boundary = Arc::new(Sphere::new(
+        &Point3::new(360.0, 150.0, 145.0), 70.0, Arc::new(Dielectric::new(1.5))));
+    world.add(boundary.clone());
+    world.add(Arc::new(ConstantMedium::new(boundary, 0.2, &Color::new(0.2, 0.4, 0.9))));
+    boundary = Arc::new(Sphere::new(
+        &Point3::new(0.0, 0.0, 0.0),
+        5000.0,
+        Arc::new(Dielectric::new(1.5)),
+    ));
+    world.add(Arc::new(ConstantMedium::new(boundary, 0.0001, &Color::white())));
+
+    let e_mat = Arc::new(Lambertian::new_tex(Arc::new(ImageTexture::new("earthmap.jpg"))));
+    world.add(Arc::new(Sphere::new(
+        &Point3::new(400.0, 200.0, 400.0),
+        100.0,
+        e_mat,
+    )));
+    let per_text = Arc::new(NoiseTexture::new(0.2));
+    world.add(Arc::new(Sphere::new(
+        &Point3::new(220.0, 280.0, 300.0),
+        80.0,
+        Arc::new(Lambertian::new_tex(per_text)),
+    )));
+
+    let mut boxes2 = HittableList::new();
+    let white = Arc::new(Lambertian::new(Color::new(0.73, 0.73, 0.73)));
+    let ns = 1000;
+    for _j in 0..ns {
+        boxes2.add(Arc::new(Sphere::new(
+            &Point3::random_in(0.0, 165.0),
+            10.0,
+            white.clone(),
+        )));
+    }
+
+    world.add(Arc::new(Translate::new(
+        Arc::new(RotateY::new(
+            Arc::new(BvhNode::from_list(&mut boxes2)), 15.0,
+        )),
+        &Vec3::new(-100.0, 270.0, 395.0),
+    )));
+
+    let world = HittableList::new_from(Arc::new(BvhNode::from_list(&mut world)));
+
+    let image_settings = ImageSettings {
+        aspect_ratio: 1.0,
+        image_width: 800,
+        quality: 100,
+        samples_per_pixel: 10000,
+        max_depth: 40,
+        background: Color::black(),
+    };
+
+    let camera_settings = CameraSettings {
+        vfov: 40.0,
+        look_from: Point3::new(478.0, 278.0, -600.0),
+        look_at: Point3::new(278.0, 278.0, 0.0),
+        vup: Vec3::new(0.0, 1.0, 0.0),
+        defocus_angle: 0.0,
+        focus_dist: 10.0,
+    };
+
+    let mut camera = Camera::new(image_settings, camera_settings);
+    camera.render(world);
+
+    println!(
+        "Output image as \"{}\"",
+        style(path.to_str().unwrap()).yellow()
+    );
+    let output_image = image::DynamicImage::ImageRgb8(camera.img);
+    let mut output_file = File::create(path).unwrap();
+    match output_image.write_to(
+        &mut output_file,
+        image::ImageOutputFormat::Jpeg(camera.quality),
+    ) {
+        Ok(_) => {}
+        Err(_) => println!("{}", style("Outputting image fails.").red()),
+    }
+
+    exit(0);
+}
