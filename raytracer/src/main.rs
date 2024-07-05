de232d92bf977f24e27d8b8ea4b571271ae65ab0
XsystemH <1319971303@qq.com>
2024-07-05 16:36:56 +0800

    Cornell box, sampling only the light, 10 samples per pixel_fmt

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -345,7 +345,7 @@ fn quads() {
 }
 
 fn cornell_box() {
-    let path = std::path::Path::new("output/book3/image8.jpg");
+    let path = std::path::Path::new("output/book3/image7.jpg");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
